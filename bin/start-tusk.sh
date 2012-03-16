6248fa337eaad1ab7877de7f0646b233aa09baf3
Justin Hayes <jhayes@redhat.com>
2012-03-15 23:14:38 -0400

    Fixed readme to add more info and update based on changes with newer version of the installed packages. The readme is geared towards (and alidated on) Fedora 16 now. Fixed order in tusk control scripts. Fixed a pom file too.

--- bin/start-tusk.sh
+++ bin/start-tusk.sh
@@ -7,6 +7,6 @@ sudo service hadoop-0.20-datanode start
 sudo service hadoop-0.20-secondarynamenode start
 sudo service hadoop-0.20-jobtracker start
 sudo service hadoop-0.20-tasktracker start
+sudo service hadoop-zookeeper-server start
 sudo service hadoop-hbase-master start
 sudo service hadoop-hbase-regionserver start
-sudo service hadoop-zookeeper-server start
--- bin/stop-tusk.sh
+++ bin/stop-tusk.sh
@@ -2,9 +2,9 @@
 
 # Stop services.
 
-sudo service hadoop-zookeeper-server stop
 sudo service hadoop-hbase-regionserver stop
 sudo service hadoop-hbase-master stop
+sudo service hadoop-zookeeper-server stop
 sudo service hadoop-0.20-tasktracker stop
 sudo service hadoop-0.20-jobtracker stop
 sudo service hadoop-0.20-secondarynamenode stop
