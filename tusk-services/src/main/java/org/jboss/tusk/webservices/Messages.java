edec985f69a31f7e54ec2db446160c4e1b3b8522
Justin Hayes <jhayes@redhat.com>
2012-07-08 15:25:10 -0400

    separated steps in big data extractor action into multiple actions.

--- tusk-services/src/main/java/org/jboss/tusk/webservices/Messages.java
+++ tusk-services/src/main/java/org/jboss/tusk/webservices/Messages.java
@@ -45,33 +45,11 @@ public class Messages {
 	}
 
 
-	//TODO this calls a rest service running on tomcat. Once we remove tomcat, it can just use the
-	//code that is in the Indexer.getZips method
+	//TODO this is just a test, with a dummy response
 	@WebMethod()
 	public @WebResult(name="zipOperationsShort", partName="zipOperationsShort") String operationsByZipShort(String zip) {
 	    System.out.println("Zip: " + zip);
-	    
-	    String restUrl = "http://localhost:8888/TuskUI/rest/indexer/zip" + (zip != null ? "/" + zip : "");
-	    
-		Client c = Client.create();
-		WebResource r = c.resource(restUrl);
-		String indexResponse = r.get(String.class);
-		
-//	    ZipOperations zos = new ZipOperations();
-//	    
-//		String[] operations = indexResponse.split("\\|");
-		System.out.println("Response from rest: " + indexResponse);
-//		for (int i = 0; i < operations.length; i++) {
-////			System.out.println("operation " + i + "=" + operations[i]);
-//			if (operations[i].indexOf(",") > -1) {
-//				String parts[] = operations[i].split(",");
-//				ZipOperation zo = new ZipOperation(parts[0], parts[1]);
-//				zos.addZipOperation(zo);
-//			}
-//		}
-//		
-//		System.out.println("Returning " + zos.getZipOperation().size() + " zip operations.");
-	    
-	    return indexResponse;
+	    	        
+	    return "<response>nothing to see here</response>";
 	}
 }
